033609da335588d1cafa9742962d9aed088676d2
KMN <kuwwatt@gmail.com>
2022-12-13 20:05:21 +0100

    notes from Github class

--- GitHub_Practice/GitPractice/src/package1/Test1.java
+++ GitHub_Practice/GitPractice/src/package1/Test1.java
@@ -1,6 +1,30 @@
 package package1;
 
 public class Test1 {
+/*
 
+//I finished my job you can update your project
+//for to see that comment we have to go Git  module and click Fetch
+
+//for copy of code click Git below(alttaki) and click Local>master==> new branch from master
+//when we create new branch we have gave them name and for that we use date and project name or ticket name-US
+//master means main project
+
+//if you work in company PLEASE CREATE YOUR OWN BRANCH THEN START WORK
+//WE ALWAYS WORK COPY MAIN PROJECT, we are not working inside the master
+//FOR EACH JOB WE CREATE COPY
+//MASTER SHOULD BE PROTECTED
+//WHEN WE FINISHED WE ARE MERGING OUR CODES TOGETHER
+//BECAUSE WE WANT TO SEE OUR CODES FROM OTHERS EYES
+
+
+//we created copy of master, and we wrote something that we unliked, and we want to delete it
+//for that we have to go master==>checkout==>then click copy and delete
+
+
+//what happened wrong copy of IDE? go to the commit and click your wrong class and rollback
+//then if you want you can rollback or delete
+
+ */
 
 }
