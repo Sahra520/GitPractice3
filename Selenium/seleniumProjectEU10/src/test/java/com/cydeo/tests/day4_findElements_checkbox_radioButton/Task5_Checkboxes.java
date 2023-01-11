ce3b1c2bf06405561f43aa741b45b98ca6259c3f
KMN <kuwwatt@gmail.com>
2023-01-11 20:48:01 +0100

    day4_findElements_checkbox_radioButton

--- Selenium/seleniumProjectEU10/src/test/java/com/cydeo/tests/day4_findElements_checkbox_radioButton/Task5_Checkboxes.java
+++ Selenium/seleniumProjectEU10/src/test/java/com/cydeo/tests/day4_findElements_checkbox_radioButton/Task5_Checkboxes.java
@@ -1,13 +1,15 @@
 package com.cydeo.tests.day4_findElements_checkbox_radioButton;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
 public class Task5_Checkboxes {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
        // Practice: Checkboxes
        // 1. Go to https://practice.cydeo.com/checkboxes
@@ -16,17 +18,29 @@ public class Task5_Checkboxes {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("https://practice.cydeo.com/checkboxes");
 
+        //We have to locate checkboxes
+        WebElement checkbox1 = driver.findElement(By.xpath("//input[@name='checkbox1']"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//input[@name='checkbox2']"));
+
        // 2. Confirm checkbox #1 is NOT selected by default
+        System.out.println("checkbox1.isSelected(), expecting false = " + checkbox1.isSelected());
 
-       // 3. Confirm checkbox #2 is SELECTED by default.
 
-       // 4. Click checkbox #1 to select it.
+        // 3. Confirm checkbox #2 is SELECTED by default.
+        System.out.println("checkbox2.isSelected() expecting true = " + checkbox2.isSelected());
+
+        // 4. Click checkbox #1 to select it.
+        Thread.sleep(3000);
+        checkbox1.click();
 
        // 5. Click checkbox #2 to deselect it.
+        Thread.sleep(3000);
+        checkbox2.click();
 
        // 6. Confirm checkbox #1 is SELECTED.
-
-       // 7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkbox1.isSelected(), expecting true = " + checkbox1.isSelected());
+        // 7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkbox2.isSelected(), expecting false = " + checkbox2.isSelected());
     }
 }
 /*
