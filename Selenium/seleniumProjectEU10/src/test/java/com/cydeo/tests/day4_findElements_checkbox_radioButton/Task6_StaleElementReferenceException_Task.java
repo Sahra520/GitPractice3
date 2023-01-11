f9e064c7582c742a212d5db7631572283914d09d
KMN <kuwwatt@gmail.com>
2023-01-11 22:16:21 +0100

    day4_findElements_checkbox_radioButton_Task6_StaleElementReferenceException_Task

--- Selenium/seleniumProjectEU10/src/test/java/com/cydeo/tests/day4_findElements_checkbox_radioButton/Task6_StaleElementReferenceException_Task.java
+++ Selenium/seleniumProjectEU10/src/test/java/com/cydeo/tests/day4_findElements_checkbox_radioButton/Task6_StaleElementReferenceException_Task.java
@@ -1,13 +1,15 @@
 package com.cydeo.tests.day4_findElements_checkbox_radioButton;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
 public class Task6_StaleElementReferenceException_Task {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
        // TC #6: StaleElementReferenceException Task
        // 1- Open a Chrome browser
@@ -20,10 +22,18 @@ public class Task6_StaleElementReferenceException_Task {
 
 
        // 3- Locate “CYDEO” link, verify it is displayed.
+        WebElement cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
-       // 4- Refresh the page.
+        Thread.sleep(3000);
 
-       // 5- Verify it is displayed, again.
+        // 4- Refresh the page.
+        driver.navigate().refresh();
+
+        // 5- Verify it is displayed, again.
+        //We are refreshing the web element reference by re-assigning(re-locating) the web element
+        cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
 
     }
 }
