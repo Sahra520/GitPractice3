ee4ef3332fae02f2d859313fd4f09b16b1e62eca
KMN <kuwwatt@gmail.com>
2023-01-15 22:19:51 +0100

    day5_task1 stale element reference exception practice

--- Selenium/seleniumProjectEU10/src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/Task1_StaleElementReferenceExample.java
+++ Selenium/seleniumProjectEU10/src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/Task1_StaleElementReferenceExample.java
@@ -2,7 +2,9 @@ package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,12 +16,37 @@ public class Task1_StaleElementReferenceExample {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
 
+        //2. Go to https://practice.cydeo.com/add_remove_elements/
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get(" https://practice.cydeo.com/add_remove_elements/");
 
-        //2. Go to https://practice.cydeo.com/add_remove_elements/
-       // driver.findElements(By.xpath());
+        //3. Click to “Add Element” button
+        //button[@onclick='addElement()']
+        WebElement addElementBtn = driver.findElement(By.xpath("//button[.='Add Element']"));
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        addElementBtn.click();
 
+        //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@class='added-manually']"));
+        System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+
+        //5. Click to “Delete” button.
+        deleteButton.click();
+
+        //6. Verify “Delete” button is NOT displayed after clicking.
+
+
+        try {
+            System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+        }catch (StaleElementReferenceException e){
+            System.out.println("-->StaleElementReferenceException exception is thrown");
+            System.out.println("-->This means the web element is completely deleted.");
+            System.out.println("deleteButton.isDisplayed() = false");
+        }
 
     }
 }
