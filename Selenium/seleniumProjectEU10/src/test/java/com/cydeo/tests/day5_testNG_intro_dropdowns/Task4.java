d894a001dd0a69e91e8a0566bb8ed01a74bb26f6
KMN <kuwwatt@gmail.com>
2023-02-14 16:03:14 +0100

    package com.cydeo.tests.day5_testNG_intro_dropdowns; Task4.java

--- Selenium/seleniumProjectEU10/src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/Task4.java
+++ Selenium/seleniumProjectEU10/src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/Task4.java
@@ -1,10 +1,58 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class Task4 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("Chrome");
+        //maximize the page
+        driver.manage().window().maximize();
+        //implicitly wait
+        driver.manage().timeouts().implicitlyWait(15000, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/dropdown");
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void simpleDropdownTest(){
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+        WebElement currentlySelectedOption = simpleDropdown.getFirstSelectedOption();
+
+        String actualSimpleDropdownText = currentlySelectedOption.getText();
+        String expectedSimpleDropdownText = "Please select an option";
+
+        Assert.assertEquals(actualSimpleDropdownText,expectedSimpleDropdownText);
+
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+
+        String expectedStateDropdownText = "Select a State";
+        String actualStateDropdownText = stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualStateDropdownText,expectedStateDropdownText);
+
+        //Assert.assertEquals(stateDropdown.getFirstSelectedOption().getText(),"Select a State");
+
+    }
 }
 /*
-
-
 TC#4: Verifying “Simple dropdown” and “State selection” dropdown
 default values
 1. Open Chrome browser
@@ -13,5 +61,4 @@ default values
 Expected: “Please select an option”
 4. Verify “State selection” default selected value is correct
 Expected: “Select a State”
-
  */
